50c4a4b19d78b1a582c2d6a974309957d508db31
cemkAHAHA <ceme4k4@mail.ru>
2022-12-17 17:20:18 +0300

    Some header additions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Осуществить вход пользователя и перенаправление на страницу профиля.
+      log_in user
+      redirect_to user
     else
-      # Выдать сообщение об ошибке.
       render 'new'
     end
   end
+
+
 end
