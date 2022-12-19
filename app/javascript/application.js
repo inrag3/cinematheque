10cb026963b3a8dc51a031d0e0ee9369751844aa
inrage <70843287+inrag3@users.noreply.github.com>
2022-12-19 16:29:55 +0300

    added logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "bootstrap"
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require turbolinks
-//= require_tree .
