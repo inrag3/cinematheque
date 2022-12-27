ef3c5c311fa20b19673ed6e284fcbc71612f623d
cemkAHAHA <ceme4k4@mail.ru>
2022-12-27 15:05:16 +0300

    Trying to integrate Kinopoisk API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
+require 'net/https'
+require 'uri'
+require 'json'
+
 class PagesController < ApplicationController
   def home
     if logged_in?
@@ -7,10 +11,16 @@ class PagesController < ApplicationController
   end
 
   def browse
+    uri = URI("https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
+    response = http.request(request)
+    @body = JSON.parse(response.body)
   end
 
   def about
 
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -68,9 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_220123) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
