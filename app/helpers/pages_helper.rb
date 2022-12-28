e80702a34738138ababe601af7da7b48cfd24897
inrage <70843287+inrag3@users.noreply.github.com>
2022-12-28 18:51:31 +0300

    Fix null rate

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -9,16 +9,6 @@ module PagesHelper
     end
   end
 
-
-  def movies_from_params(params)
-    uri = URI("https://kinopoiskapiunofficial.tech/api/v2.2/films/top")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.path + '?' + params.map{|k,v| "#{k}=#{v}"}.join("&"), {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
-    response = http.request(request)
-    JSON.parse(response.body)
-  end
-
   def top
     uri = URI(URL + "/top?")
     api_request(uri)
