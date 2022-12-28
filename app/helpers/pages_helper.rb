71a6fe7f5274b4e807a24501e5916f827f288507
inrage <70843287+inrag3@users.noreply.github.com>
2022-12-28 23:00:45 +0300

    Revert "Refactoring method vie api"
    This reverts commit 70371f1db3866db3c90e8fa699430aac565425a1.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
-  URL = "https://kinopoiskapiunofficial.tech/api/v2.2/films"
+  URL = "https://kinopoiskapiunofficial.tech/api/v2.1/films"
   def full_title(page_title = '')
     base_title = "Cinematheque"
     if page_title.empty?
@@ -10,25 +10,19 @@ module PagesHelper
   end
 
   def top
-    params[:page] ||= '1'
-    @current_browse_page = params[:page]
     uri = URI(URL + "/top?")
     api_request(uri)
   end
 
   def movies
-    params[:page] ||= '1'
-    @current_browse_page = params[:page]
     uri = URI(URL + "/search-by-keyword?")
     api_request(uri)
   end
 
   def id
-    uri = URI(URL + "/#{params[:id]}")
-    params[:id] = nil
+    uri = URI(URL + '/')
     api_request(uri)
   end
-
   def get_type_from_rate(vote)
     if vote.to_f >= 7
       return "green"
@@ -40,13 +34,14 @@ module PagesHelper
 
   private
   def api_request(uri)
+    @current_browse_page = params[:page] || '1'
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    p = { type: params[:type], id: params[:id], keyword: params[:search], page: params[:page] }
+    p = { type: params[:type], id: params[:id], keyword: params[:search], page: @current_browse_page,  }
     p.delete_if{|_,v| v.nil? }
-    pp uri.to_s + p.map{|k,v| "#{k}=#{CGI.escape(v)}"}.join("&")
     request = Net::HTTP::Get.new(uri.to_s + p.map{|k,v| "#{k}=#{CGI.escape(v)}"}.join("&"), {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
     response = http.request(request)
+    pp uri.to_s + p.map{|k,v| "#{k}=#{CGI.escape(v)}"}.join("&")
     JSON.parse(response.body)
   end
 
