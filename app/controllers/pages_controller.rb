5c438e862eca0ed75db6e44764110117795d6453
inrage <70843287+inrag3@users.noreply.github.com>
2022-12-28 18:20:12 +0300

    Add api search and pagination partitial view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ require 'cgi'
 
 class PagesController < ApplicationController
   include PagesHelper
+
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
@@ -13,18 +14,12 @@ class PagesController < ApplicationController
   end
 
   def browse
-    @current_browse_page = params[:page] || '1'
-    p = { :type => 'TOP_250_BEST_FILMS', :page => @current_browse_page}
-    @movies = movies_from_params(p)
+    params[:type] = "TOP_250_BEST_FILMS"
+    @movies = top
   end
 
   def find
-    uri = URI("https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.to_s + CGI.escape(params[:search]), {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
-    response = http.request(request)
-    @movies = JSON.parse(response.body)
+    @movies = movies
   end
 
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,5 @@
 module PagesHelper
+  URL = "https://kinopoiskapiunofficial.tech/api/v2.1/films"
   def full_title(page_title = '')
     base_title = "Cinematheque"
     if page_title.empty?
@@ -18,6 +19,20 @@ module PagesHelper
     JSON.parse(response.body)
   end
 
+  def top
+    uri = URI(URL + "/top?")
+    api_request(uri)
+  end
+
+  def movies
+    uri = URI(URL + "/search-by-keyword?")
+    api_request(uri)
+  end
+
+  def id
+    uri = URI(URL + '/')
+    api_request(uri)
+  end
   def get_type_from_rate(vote)
     if vote.to_f >= 7
       return "green"
@@ -25,6 +40,19 @@ module PagesHelper
       return "orange"
     end
     "red"
+  end
+
+  private
+  def api_request(uri)
+    @current_browse_page = params[:page] || '1'
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    p = { type: params[:type], id: params[:id], keyword: params[:search], page: @current_browse_page,  }
+    p.delete_if{|_,v| v.nil? }
+    request = Net::HTTP::Get.new(uri.to_s + p.map{|k,v| "#{k}=#{CGI.escape(v)}"}.join("&"), {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
+    response = http.request(request)
+    pp uri.to_s + p.map{|k,v| "#{k}=#{CGI.escape(v)}"}.join("&")
+    JSON.parse(response.body)
   end
 
 end
