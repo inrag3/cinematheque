70371f1db3866db3c90e8fa699430aac565425a1
inrage <70843287+inrag3@users.noreply.github.com>
2022-12-28 22:56:00 +0300

    Refactoring method vie api

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,11 +1,7 @@
 class ContentsController < ApplicationController
+  include PagesHelper
   def show
-    uri = URI("https://kinopoiskapiunofficial.tech/api/v2.2/films/")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.path + params[:id], {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
-    response = http.request(request)
-    @content = JSON.parse(response.body)
+    @content = id
   end
 
 end
